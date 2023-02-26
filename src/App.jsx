9517d2b0836de88bdd595bf70ae92a0246484a71
0rigin-c0de <shunnu807@gmail.com>
2023-02-26 22:25:42 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Camera from "./components/Camera";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Camera />
+    </div>
+  );
+}
+
+export default App;
