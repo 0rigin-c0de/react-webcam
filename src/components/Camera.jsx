9404f7d8956196235333c4b5f5f52a7cadd7f6bf
0rigin-c0de <shunnu807@gmail.com>
2023-06-11 10:14:39 +0530

    update the ui and styles

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,25 +1,59 @@
-import React, { useRef } from "react";
+import React from "react";
 import Webcam from "react-webcam";
 import "../index.css";
 
-const Camera = () => {
-  const webcamRef = useRef(null);
+const App = () => {
+  const webcamRef = React.useRef(null);
+  const [capturedImage, setCapturedImage] = React.useState(null);
 
   const capture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
-    console.log(imageSrc);
+    setCapturedImage(imageSrc);
+  };
+
+  const retake = () => {
+    setCapturedImage(null);
   };
 
   return (
-    <div className="container">
-      <div className="camera">
-        <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+    <div className="app-container">
+      <h1 className="app-title">React Camera App</h1>
+      <div className="camera-container">
+        {capturedImage ? (
+          <img src={capturedImage} alt="Captured" className="captured-image" />
+        ) : (
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            mirrored={true} // Add this line to mirror the camera
+            className="webcam"
+          />
+        )}
       </div>
-      <button className="button" onClick={capture}>
-        Capture photo
-      </button>
+      <div className="button-container">
+        {!capturedImage && (
+          <button onClick={capture} className="capture-button">
+            Capture
+          </button>
+        )}
+        {capturedImage && (
+          <div>
+            <button onClick={retake} className="retake-button">
+              Retake
+            </button>
+            <a
+              href={capturedImage}
+              download="captured-image.jpg"
+              className="download-link"
+            >
+              Download
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
-export default Camera;
+export default App;
